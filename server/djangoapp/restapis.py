2d11669816286ee142280add646b80d4b5f6959e
Luca Ionut-Catalin <112466341+IonutLuca86@users.noreply.github.com>
2025-02-18 21:00:05 +0100

    and again

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -38,7 +38,7 @@ def analyze_review_sentiments(text):
     except Exception as err:
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
-        
+
 
 def post_review(data_dict):
     request_url = backend_url+"/insert_review"
